e2b3dea4385af79985fe331326038a1dbb8c9f9f
azim679 <163021300+azim679@users.noreply.github.com>
2025-03-07 14:45:33 +0000

    Update settings.py

--- SmartMed/settings.py
+++ SmartMed/settings.py
@@ -40,13 +40,11 @@ SECRET_KEY = 'django-insecure-e)359z=w#b9=b00d3dq#y+#@wcp02sj+&-9vdiw+ez3es8@rci
 DEBUG = True
 
 
-PORT = os.getenv("PORT")  
 
 ALLOWED_HOSTS = [
     "smartmedproject.onrender.com",
     "localhost",
     "127.0.0.1",
-    "0.0.0.0",  # Add this!
 ]
 
 CSRF_TRUSTED_ORIGINS = [
